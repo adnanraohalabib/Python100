721a74bc1a98448ff44701857a814898c7f48177
labib <theloserbd1@gmail.com>
2024-03-09 16:47:28 +0600

    first commitment to complete 100 days

---
+++ day 1 - brand name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to brand name generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your Brand name could be {city} {pet}")
